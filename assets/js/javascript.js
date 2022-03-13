0e678efe102afcf4361fc4e5a805f5922d62c7fa
caiomath <caiomralves.ca@gmail.com>
2022-03-13 15:54:26 -0300

    upload do projeto

---
+++ assets/js/javascript.js
@@ -0,0 +1,23 @@
+
+window.sr = ScrollReveal({ reset: true});
+sr.reveal('.cardquad1', {duration: 1000});
+sr.reveal('.cardquad2', {duration: 1500});
+sr.reveal('.cardquad3', {duration: 2000});
+sr.reveal('.contact-1', {duration: 1000});
+sr.reveal('.contact-2', {duration: 1300});
+sr.reveal('.whatsme', {duration: 1200});
+sr.reveal('.leia-sobre', {duration: 1400});
+sr.reveal('.port-text', {duration: 1200});
+sr.reveal('.box1', {duration: 1000});
+sr.reveal('.box2', {duration: 1500});
+sr.reveal('.estudos-title', {duration: 1000});
+sr.reveal('.slider', {duration: 1300});
+sr.reveal('.linex', {duration: 1600});
+sr.reveal('.footer-text', {duration: 1000});
+
+// function focoModal(){
+//     let ancorx = document.querySelector('.aba1');
+//     ancorx.focus();
+// }
+
+// focoModal();
